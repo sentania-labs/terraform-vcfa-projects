04b47c17012b1c1b1257d68a278de7a9e8251b06
Scott R Bowe <scottb@sentania.net>
2025-12-17 10:16:38 -0600

    fmt

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.14.0"
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = ">= 3.0.0"
     }
     vcfa = {
-      source = "vmware/vcfa"
+      source  = "vmware/vcfa"
       version = ">= 1.0.0"
     }
   }
